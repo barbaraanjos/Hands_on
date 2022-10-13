2287d65af1af8e11ed3fd38c5266e26301d46fd5
Barbara Anjos <ba.dosanjos@gmail.com>
2022-10-13 01:34:07 -0300

    delet

--- Atividade_5/friendnew.rb
+++
@@ -1,8 +0,0 @@
-name = ['yoda', 'maria']
-#friends = ['yoda', 'maria']#["joao", "pedro", "ricardo", yoda, maria]
-
-def friend(friends)
-    friends.select { |name| name.length == 4 }
-end
-
-puts friend
